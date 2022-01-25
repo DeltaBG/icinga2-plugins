b4c8090b20ccf6ddee1f2b898b26030d6c42bcc3
Nedelin Petkov <nedelin.petkov@delta.bg>
2022-01-25 15:29:14 +0200

    Updated common-plugins/check_ownership.sh

--- common-plugins/check_ownership.sh
+++ common-plugins/check_ownership.sh
@@ -15,6 +15,7 @@ help () {
     echo -e "\t-d\tFull path to the directory."
     echo -e "\t-u\tUser name of the owner."
     echo -e "\t-g\tGroup name of the owner."
+    echo -e "\t-s\tShow list of files and directories."
     echo
     echo -e "\t-h\tHelp"
     exit $UNKNOWN
@@ -24,25 +25,36 @@ FIND_BIN=`which find 2>/dev/null`
 
 [ ! -f "$FIND_BIN" ] && echo "ERROR: You must have 'find'." && help
 
+SHOW=false
+
 # Getting parameters:
-while getopts "d:u:g:h" OPT; do
+while getopts "d:u:g:sh" OPT; do
     case $OPT in
-        "d") DIR=$OPTARG;;
-        "u") USER=$OPTARG;;
-        "g") GROUP=$OPTARG;;
-        "h") help;;
+        d ) DIR=$OPTARG;;
+        u ) USER=$OPTARG;;
+        g ) GROUP=$OPTARG;;
+        s ) SHOW=true;;
+        h ) help;;
+        \?) echo "Unknown option!"; help;;
+        : ) echo "Missing option arguments."; help;;
+        * ) echo "Unimplemented option"; help;;
     esac
 done
 
 ( [ -z "$DIR" ] || [ -z "$USER" ] || [ -z "$GROUP" ] ) && \
     echo "ERROR: You must specify a directory, user, and group." && help
 
-OUTPUT=`eval "$FIND_BIN $DIR ! -user $USER -or ! -group $GROUP"`
+OUTPUT=$(eval "$FIND_BIN $DIR ! -user $USER -or ! -group $GROUP")
 
 if [ -z "$OUTPUT" ]; then
     echo "OK: No files or directories with permissions other than user '$USER' or group '$GROUP'."
     exit $OK
 else
-    echo "WARNING: Found '$(echo $OUTPUT | wc -l)' files or directories with permissions other than user '$USER' or group '$GROUP'."
+    NUM_FOUND=$(echo -e $OUTPUT | sed 's/ /\n/' | wc -l)
+    echo "WARNING: Found '$NUM_FOUND' files or directories with permissions other than user '$USER' or group '$GROUP'."
+    if $SHOW; then
+        echo Found:
+        echo -e $OUTPUT | sed 's/ /\n/'
+    fi
     exit $WARNING
 fi
