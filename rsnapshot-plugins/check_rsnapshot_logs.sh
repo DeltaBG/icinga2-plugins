e073b67e93b34835d5b4bc96d9550944f9a595ee
Dean Iliev <dean.iliev@delta.bg>
2023-12-01 10:26:24 +0200

    Changed failed and warning logic

--- rsnapshot-plugins/check_rsnapshot_logs.sh
+++ rsnapshot-plugins/check_rsnapshot_logs.sh
@@ -59,8 +59,8 @@ WARN_BACKUPS=0
 LEVEL=0
 
 for i in $(ls $LOGS); do
-	FAILED=`grep ": completed" $i | tail -n ${COUNT} | grep "error" | wc -l`
-	WARNS=`grep ": completed" $i | tail -n ${COUNT} | grep "warning" | wc -l`	
+	FAILED=`grep "ERROR:" $i | tail -n ${COUNT} | wc -l`
+	WARNS=`grep "WARNING:" $i | tail -n ${COUNT} | wc -l`
 	FAILED_BACKUPS=$(( $FAILED_BACKUPS + $FAILED ))
 	WARN_BACKUPS=$(( $WARN_BACKUPS + $WARNS ))
 done
