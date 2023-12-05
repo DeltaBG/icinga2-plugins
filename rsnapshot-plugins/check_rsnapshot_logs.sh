3227f9f2d10610897ce5040f521592ddae5cd9dd
Dean Iliev <dean.iliev@delta.bg>
2023-12-05 10:00:16 +0200

    Changed rsnapshot plugin count logic

--- rsnapshot-plugins/check_rsnapshot_logs.sh
+++ rsnapshot-plugins/check_rsnapshot_logs.sh
@@ -59,8 +59,8 @@ WARN_BACKUPS=0
 LEVEL=0
 
 for i in $(ls $LOGS); do
-	FAILED=`grep -i "error" $i | tail -n ${COUNT} | wc -l`
-	WARNS=`grep -i "warning" $i | tail -n ${COUNT} | wc -l`
+	FAILED=`grep "completed" | grep -i "error" $i | tail -n ${COUNT} | wc -l`
+	WARNS=`grep "completed" | grep -i "warning" $i | tail -n ${COUNT} | wc -l`
 	FAILED_BACKUPS=$(( $FAILED_BACKUPS + $FAILED ))
 	WARN_BACKUPS=$(( $WARN_BACKUPS + $WARNS ))
 done
